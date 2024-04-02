219402688784fd5a1b33ad7004b6f812e0688dba
안소희 <2022112701@dgu.ac.kr>
2024-04-02 22:22:58 +0900

    answer added

---
+++ next-auth-demo/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex justify-center items-center">
+      <h1>Next.js. Auth Tutorial</h1>
+    </main>
+  );
+}
